f61a3d4193a133fb95d7f5662c8367b4d36e04f5
kangg4207 <kangg4207@CA-SU-F106-34.sduhsd.lan>
2020-02-13 11:04:07 -0800

    First Commit, 02-13-2020, School

---
+++ APCSA/src/consumerLab/Main.java
@@ -0,0 +1,25 @@
+package consumerLab;
+import java.io.File;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+	//test reading from files
+	//read in the positive adjectives in postiveAdjectives.txt
+	/*try {
+		Scanner input = new Scanner(new File("src/consumerLab/positiveAdjectives.txt"));
+		while(input.hasNextLine()){
+			String temp = input.nextLine().trim();
+			System.out.println(temp);
+		}
+		
+		input.close();
+	}
+	catch(Exception e){
+		System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
+	}*/
+	
+	System.out.println(Review.sentimentVal("goods"));
+	System.out.println(Review.totalSentiment("src/consumerLab/simpleReview.txt"));
+  }
+}
