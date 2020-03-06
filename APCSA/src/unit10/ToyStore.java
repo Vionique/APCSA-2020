33210d3c9da93154feeabc94405d76ddda7e5759
Vionique <60482689+Vionique@users.noreply.github.com>
2020-03-06 09:28:37 -0800

    Update ToyStore, 03-06-2020

--- APCSA/src/unit10/ToyStore.java
+++ APCSA/src/unit10/ToyStore.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import static java.lang.System.*;
 
 public class ToyStore
@@ -51,7 +52,11 @@ public class ToyStore
   	}  
   	
   	public ArrayList<Toy> Count() {
-  		ArrayList<Toy> inventory = new ArrayList<Toy>();
+  		ArrayList<Toy> inventoryOne = new ArrayList<Toy>();
+  		//ArrayList<Toy> inventoryEnd = new ArrayList<Toy>();
+  		//inventoryEnd.add(new Toy("placeholder"));
+  		//inventoryEnd.add(new Toy("placeholder"));
+  		ArrayList<Integer> remove = new ArrayList<Integer>();
   		
   		for (int i = 0; i < toyList.size(); i++) {
   			int count = 0;
@@ -60,30 +65,46 @@ public class ToyStore
   	  				count++;
   			}
   			
-  			inventory.add(new Toy("gi joe", count));
+  			inventoryOne.add(new Toy(toyList.get(i).getName(), count));
   		}
-  		//printInventory(inventory);
   		
-  		for (int j = 1; j < inventory.size(); j++){
-  			for (int k = 0; k <= j) {
-  			String compOne = inventory.get(j).getName();
-  			String compTwo = inventory.get(j - 1).getName();
-  			if (compOne.equals(compTwo)) {
-  				
+  		HashSet<Toy> inventoryEnd = new HashSet(inventoryOne);
+  		/*for (Toy t : inventoryOne) { 
+  			for (int i = 0; i < inventoryEnd.size(); i++) {
+  				if (i > 0)
+  				{
+	  				String one = inventoryEnd.get(i - 1).getName();
+	  				String two = t.getName();
+	  				if (!(one.equals(two)))
+	  					inventoryEnd.add(i, t);
+  				}
+  					
+  				else 
+  					inventoryEnd.set(1, inventoryOne.get(0));
   			}
-  			}
+  			
+  			
+        }
+  		
+  		inventoryEnd.remove(0); 
+  		System.out.println(inventoryEnd.size());
+  		//printInventory(inventoryEnd);*/
+  		ArrayList<Toy> inv = new ArrayList<Toy>();
+  		for (Toy t : inventoryEnd) {
+  			inv.add(t);
   		}
-  		return inventory;
+  		printInventory(inv);
+  		return inv;
   		
   	}
   	//prints Inventory
-  	/*public void printInventory(ArrayList<Toy> inventory) {
+  	public void printInventory(ArrayList<Toy> inventory) {
   		String list = "inventory:";
 		for (Toy t : inventory) {
 			list = list + "\n" + t.getName() + " " + t.getCount();
 		}
 		System.out.println( list );
-  	}*/
+  	}
   	
   	
 	public String toString()
