5292dedf4135bfd28339aaec497bd175832848c2
Vionique <gabbykang02@gmail.com>
2020-03-06 11:06:54 -0800

    Lab initialization, 03-06-2020

---
+++ APCSA/src/celebLab/SimpleGUIRunner.java
@@ -0,0 +1,14 @@
+package celebLab;
+
+
+import javax.swing.JFrame;
+
+public class SimpleGUIRunner
+{
+	public static void main(String[] args)
+	{
+		JFrame sample = new JFrame();
+		sample.setVisible(true);
+	}
+
+}
