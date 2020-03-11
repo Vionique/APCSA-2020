04a6511b2c58d7051ddd19f0b8f1ccbafd718385
kangg4207 <gabbykang02@gmail.com>
2020-03-11 11:43:57 -0700

    03-11-2020, First Commit, School

--- APCSA/src/dataLab/Welcome01.java
+++ APCSA/src/dataLab/Welcome01.java
@@ -5,7 +5,8 @@ import core.data.*;
 
 public class Welcome01 {
    public static void main(String[] args) {
-      String id = "KATL";
+      //String id = "KATL";
+	   String id = "KNKX";
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml"); 
       ds.setCacheTimeout(15 * 60);  
       ds.load();
@@ -13,7 +14,6 @@ public class Welcome01 {
       float temp = ds.fetchFloat("temp_f");
       String loc = ds.fetchString("location");
       System.out.println("The temperature at " + loc + " is " + temp + "F");
+
    }
 }
-
-
