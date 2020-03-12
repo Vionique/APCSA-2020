72c557b9220bf08065b4e02ba913b6e7bb9ffead
kangg4207 <gabbykang02@gmail.com>
2020-03-12 11:09:54 -0700

    Fix load

--- APCSA/src/dataLab/RestaurantRunner.java
+++ APCSA/src/dataLab/RestaurantRunner.java
@@ -10,11 +10,14 @@ public class RestaurantRunner{
 		DataSource ds = DataSource.connect("https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.xml?accessType=DOWNLOAD").load();
 		//DataSource ds = DataSource.connect("https://data.baltimorecity.gov/resource/k5ry-ef3g/" + "row-qd32-u32j.7y49").load();
 		//System.out.println(ds.fetchString("row/row/name"));
-		ds.printUsageString();
+		//ds.printUsageString();
 
-		/*ArrayList<RestaurantData> allRest = ds.fetchList(RestaurantData.class, "restaurant/name", 
-	             "restaurant/neighborhood", "restaurant/policedistrict",
-	             "restaurant/zipcode", "restaurant/councildistrict");*/
+		ArrayList<RestaurantData> allRest = ds.fetchList(RestaurantData.class, "row/row/name", 
+	             "row/row/neighborhood", "row/row/policedistrict",
+	             "row/row/zipcode", "row/row/councildistrict");
+
+			System.out.println(ds.fetchString(allRest.get(0).getName()));
+
 	}
 }
 
