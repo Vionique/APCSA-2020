5c0ca2c52ee70ee8d6dd49b25d2fcf07076c3687
kangg4207 <gabbykang02@gmail.com>
2020-03-12 11:04:34 -0700

    Fix

--- APCSA/src/dataLab/RestaurantRunner.java
+++ APCSA/src/dataLab/RestaurantRunner.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 public class RestaurantRunner{
 	
 	public static void main(String[] args) {
-		//DataSource ds = DataSource.connect("https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.xml?accessType=DOWNLOAD");
-		DataSource ds = DataSource.connect("https://data.baltimorecity.gov/resource/k5ry-ef3g/" + "rows" + ".xml").load();
-		System.out.println(ds.fetchString("rows/name"));
-		//ds.printUsageString();
+		DataSource ds = DataSource.connect("https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.xml?accessType=DOWNLOAD").load();
+		//DataSource ds = DataSource.connect("https://data.baltimorecity.gov/resource/k5ry-ef3g/" + "row-qd32-u32j.7y49").load();
+		//System.out.println(ds.fetchString("row/row/name"));
+		ds.printUsageString();
 
 		/*ArrayList<RestaurantData> allRest = ds.fetchList(RestaurantData.class, "restaurant/name", 
 	             "restaurant/neighborhood", "restaurant/policedistrict",
