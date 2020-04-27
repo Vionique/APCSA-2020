5bc9c6df12067968be961bec04c5b2db8d58fe34
Vionique <gabbykang02@gmail.com>
2020-04-27 10:01:04 -0700

    04-27-2020, Ball and Block Objects

--- APCSA/src/pong/Ball.java
+++ APCSA/src/pong/Ball.java
@@ -19,43 +19,69 @@ public class Ball extends Block
 	}
 
 	//add the other Ball constructors
+	public Ball(int x, int y) {  
+		super (x, y);
+		xSpeed = 3;
+		ySpeed = 1;
+	}
 	
+	public Ball (int x, int y, int w, int h) {
+		super (x, y, w, h);
+		xSpeed = 3;
+		ySpeed = 1;
+	}
 	
+	public Ball(int x, int y, int w, int h, Color c) {
+		super (x, y, w, h, c);
+		xSpeed = 3;
+		ySpeed = 1;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	   
+	public Ball (int x, int y, int w, int h, Color c, int xS, int yS) {
+		super (x, y, w, h, c);
+		xSpeed = xS;
+		ySpeed = yS;
+	}
    //add the set methods
    
-
+	public void setxSpeed(int x) {
+		xSpeed = x;
+	}
+	public void setySpeed(int y) {
+		ySpeed = y;
+	}
    public void moveAndDraw(Graphics window)
    {
    	//draw a white ball at old ball location
 
+	   super.draw(window, Color.white);
 
-      setX(getX()+xSpeed);
+	   setX(getX()+xSpeed);
 		//setY
-
+	   setY(getY()+ySpeed);
 		//draw the ball at its new location
+	   super.draw(window);
    }
    
 	public boolean equals(Object obj)
 	{
-
-
-
-
+		Ball testBall = (Ball) obj;
+		if (super.equals(obj) && this.getxSpeed() == testBall.getxSpeed()
+				&& this.getySpeed() == testBall.getySpeed())
+			return true;
 		return false;
 	}   
 
    //add the get methods
-
+	public int getxSpeed() {
+		return xSpeed;
+	}
+	public int getySpeed() {
+		return ySpeed;
+	}
    //add a toString() method
+	public String toString() {
+		return super.toString() + "  " + getxSpeed() + " " + getySpeed();
+	}
 }
+
--- APCSA/src/pong/Block.java
+++ APCSA/src/pong/Block.java
@@ -17,10 +17,31 @@ public class Block implements Locatable
 
 	public Block()
 	{
-
+		setColor(Color.black);
+		setPos(100,200);
+		setWidth(20);
+		setHeight(20);
 
 	}
-
+	public Block(int x, int y) {
+		setColor(Color.black);
+		setPos(x, y);
+		setWidth(20);
+		setHeight(20);
+	}
+	public Block(int x, int y, int w, int h) {
+		setColor(Color.black);
+		setPos(x, y);
+		setWidth(w);
+		setHeight(h);
+		
+	}
+	public Block(int x, int y, int w, int h, Color c) {
+		setColor(c);
+		setPos(x, y);
+		setWidth(w);
+		setHeight(h);
+	}
 	//add other Block constructors - x , y , width, height, color
 	
 	
@@ -28,46 +49,85 @@ public class Block implements Locatable
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
    //add the other set methods
-   
-
+	public void setPos(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+	@Override
+	public void setX(int x) {
+		xPos = x;
+		
+	}
+	@Override
+	public void setY(int y) {
+		yPos = y;
+		
+	}
+	public void setWidth(int w) {
+		width = w;
+	}
+	public void setHeight(int h) {
+		height = h;
+	}
    public void setColor(Color col)
    {
-
-
+	   color = col;
    }
 
    public void draw(Graphics window)
    {
    	//uncomment after you write the set and get methods
-      //window.setColor(color);
-      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+      window.setColor(color);
+      window.fillRect(getX(), getY(), getWidth(), getHeight());
    }
 
    public void draw(Graphics window, Color col)
    {
-
+	   window.setColor(col);
+	   window.fillRect(getX(), getY(), getWidth(), getHeight());
 
    }
    
 	public boolean equals(Object obj)
 	{
-
-
-
+		Block compBlock = (Block) obj;
+		if (compBlock.getColor().equals(this.getColor()) && compBlock.getX() == this.getX()
+				&& compBlock.getWidth() == this.getWidth() 
+				&& compBlock.getHeight() == this.getHeight())
+			return true;
 
 		return false;
+	}
+
+	
+	@Override
+	public int getX() {
+		return xPos;
+	}
+
+	@Override
+	public int getY() {
+		return yPos;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public Color getColor() {
+		return color;
 	}   
 
    //add the other get methods
     
-
-   //add a toString() method  - x , y , width, height, color
+	public String toString() {   //add a toString() method  - x , y , width, height, color
+		
+		return xPos + " , " + yPos + " , " + width + ", " + height + ", " + color;
+	}
 }
+
