0e6fac5a4cf5e3556a33638c0e94139d89017d3a
Vionique <gabbykang02@gmail.com>
2020-05-27 10:54:43 -0700

    UpgradeScreen Setup

---
+++ APCSA/src/finalProject/UpgradeScreen.java
@@ -0,0 +1,14 @@
+package finalProject;
+
+import javax.swing.JPanel;
+
+public class UpgradeScreen extends JPanel{
+	
+	private Homescreen frame;
+	public UpgradeScreen(Homescreen j) {
+		frame = j;
+	}
+}
+
+
+
